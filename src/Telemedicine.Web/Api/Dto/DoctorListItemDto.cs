8e9a3f0362e1f7ed3d50bb74628d4c4d26f0e0e6
Vyacheslav Borisov <borisov@itasu.ru>
2015-07-19 14:30:06 +0400

    doctor list item

---
+++ src/Telemedicine.Web/Api/Dto/DoctorListItemDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Telemedicine.Web.Api.Dto
+{
+    public class DoctorListItemDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Specialization { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        public string StatusText { get; set; }
+
+        public string StatusName { get; set; }
+
+        public bool IsChatAvailable { get; set; }
+
+        public bool IsAudioAvailable { get; set; }
+
+        public bool IsVideoAvailable { get; set; }
+    }
+}
