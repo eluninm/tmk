4a6892831fbf71d7ed1d80482a1eb8978ba48878
Safronov Kirill <kirsanv43@hotmail.com>
2015-08-10 14:15:31 +0400

    На странице "Баланс" в профиле доктора стоимость и время консультации отображается неверно #138

---
+++ src/Telemedicine.Web/Api/CustomJsonConverters/CustomDateTimeConverter.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace Telemedicine.Web.Api.CustomJsonConverters
+{
+    class CustomDateTimeConverter : IsoDateTimeConverter
+    {
+        public CustomDateTimeConverter()
+        {
+            base.DateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+        }
+    }
+}
--- src/Telemedicine.Web/Api/Dto/PaymentHistoryDto.cs
+++ src/Telemedicine.Web/Api/Dto/PaymentHistoryDto.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 using Telemedicine.Core.Models.Enums;
+using Telemedicine.Web.Api.CustomJsonConverters;
 
 namespace Telemedicine.Web.Api.Dto
 {
     public class PaymentHistoryDto
     {
-       public  DateTime Date { get; set; }
-       public string PaymentType { get; set; }
-       public double Value { get; set; }
-       public string PatientTitle { get; set; }
+        [JsonConverter(typeof (CustomDateTimeConverter))]
+        public DateTime Date { get; set; }
+
+        public string PaymentType { get; set; }
+        public double Value { get; set; }
+        public string PatientTitle { get; set; }
     }
 }
